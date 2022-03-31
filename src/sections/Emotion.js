62f9bfec3af83cc510502e12b235c040c16bee05
kimgungo <yuyukgh@naver.com>
2022-03-31 17:50:06 +0900

    Feat : Add Emotion content

--- src/sections/Emotion.js
+++ src/sections/Emotion.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import styled from "@emotion/styled";
-import { keyframes, jsx, css } from "@emotion/react";
 import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+import {keyframes} from '@emotion/react';
 
 const Container = styled.div`
   display: flex;
@@ -20,7 +20,7 @@ const rotateAnimation = keyframes`
 const Box = styled.div`
   width: 100px;
   height: 100px;
-  background-color: ${({ bgColor }) => bgColor};
+  background-color: ${({ background_color }) => background_color};
   animation: ${rotateAnimation} 1s linear infinite;
 `;
 
@@ -31,7 +31,7 @@ const Circle = styled(Box)`
 
 const Btn = styled.button`
   padding: 10px 20px;
-  background-color: ${({ bg }) => bg};
+  background-color: ${({ background_color }) => background_color};
   color: #fff;
   display: flex;
   align-items: center;
@@ -40,52 +40,39 @@ const Btn = styled.button`
   cursor: pointer;
 `;
 
-const pinkInput = css`
-  background-color: pink;
-`;
-const RedPasswordInput = (props) => (
-  <input
-    type="password"
-    css={css`
-      background-color: red;
-      display: block;
-    `}
-    {...props}
-  />
-);
+// const TextInput = styled.input.attrs({ required: true, type: "text" })`
+//   background-color: #fff;
+//   border: 1px solid #ddd;
+//   font-size: 1rem;
+//   padding: 10px;
+//   box-sizing: border-box;
+// `;
 
-const TextInput = styled.input`
-  background-color: #fff;
-  border: 1px solid #ddd;
-  padding: 10px;
-  box-sizing: border-box;
-`;
-
-const Main = () => {
+const Emotion = () => {
   const [x, setX] = useState(false);
   return (
     <>
       <Container>
-        <RedPasswordInput css={pinkInput} />
-        <TextInput required type="password" />
-        <Box bgColor="black" />
-        <Circle bgColor="tomato" />
-        <Box bgColor="green" />
-        <Circle bgColor="yellow" />
+        <Box background_color="black" />
+        <Circle background_color="tomato" />
+        <Box background_color="green" />
+        <Circle background_color="yellow" />
       </Container>
 
       <Container>
-        <Btn onClick={() => setX(!x)} bg={x ? "red" : "black"}>
+        <Btn onClick={() => setX(!x)} background_color={x ? "red" : "black"}>
           login
         </Btn>
-        <Btn to="/" as={Link} bg="blue">
+        <Btn to="/" as={Link} background_color="red">
           Home
         </Btn>
       </Container>
 
-      <Container as="form"></Container>
+      <Container as="form">
+        {/* <TextInput /> */}
+      </Container>
     </>
   );
 };
 
-export default Main;
+export default Emotion;
