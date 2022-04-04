e090915ea6e5de5280d0fee72c60c6da36089355
kimgungo <yuyukgh@naver.com>
2022-04-04 11:10:57 +0900

    Chore : react 18->17 downgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
 import { jsx, ThemeProvider } from "@emotion/react";
 import { darkTheme, lightTheme } from "./theme";
 
-const root = createRoot(document.getElementById("root") as HTMLDivElement);
-root.render(
+ReactDOM.render(
+  <React.StrictMode>
   <BrowserRouter>
     <ThemeProvider theme={lightTheme}>
       <App />
     </ThemeProvider>
   </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
